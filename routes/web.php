11fe099e77d1b371dc4466aec70de4806beeff89
mohamedsafi7 <mohamedsafi.texting@gmail.com>
2024-03-13 16:53:54 +0100

    routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\PaymentController;
 
 
 
@@ -10,6 +12,10 @@ Route::get('/', function () {
 })->middleware(['auth:sanctum', 'verified'])->name('welcome');
 
 
+
+
+Route::get('/students', [StudentController::class, 'show'])->name('students.show');
+Route::get('/payments', [PaymentController::class, 'show'])->name('payments.show');
 
 Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [UserController::class, 'registerUser']);
